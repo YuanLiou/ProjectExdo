009de5d9a1edcdbdabffb96409e13a008a7abcfe
Ray <louis383@gmail.com>
2017-08-03 23:44:10 +0800

    Jump and basic Running.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,18 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-
 	public float moveSpeed;
 	public float jumpForce;
+	
+	// Ground checking properties
+	public bool isOnGround;
+	public LayerMask whatIsGround;
 
 	private Rigidbody2D myRigidbody;
+	private Collider2D myCollider;
 
 	// Use this for initialization
 	void Start () {
+		myRigidbody = GetComponent<Rigidbody2D>();
+		myCollider = GetComponent<Collider2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		// Detect the ground
+		isOnGround = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
 		
+		// Moving right
+		myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
+		
+		// Jump (Space and left key of mouse)
+		if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
+			if (isOnGround) {
+                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+			}
+		}
 	}
 }
