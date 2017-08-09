a039c310ac8b221f40a43b3e4629c633808de4d7
Ray <louis383@gmail.com>
2017-08-09 23:57:26 +0800

    infinity generating platforms.

---
+++ Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour {
+
+	public GameObject platform;
+	public Transform platformGenerationPoint;
+	public float distanceBetweenPlatform;
+
+	private float platformWidth;
+	
+	// Use this for initialization
+	void Start () {
+		platformWidth = platform.GetComponent<BoxCollider2D>().size.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x < platformGenerationPoint.position.x) {
+			transform.position = new Vector3(transform.position.x + platformWidth + distanceBetweenPlatform, transform.position.y, transform.position.z);
+			
+			// Generate new platform
+			Instantiate(platform, transform.position, transform.rotation);
+		}
+	}
+}
