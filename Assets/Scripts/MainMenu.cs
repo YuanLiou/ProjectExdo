131e3d27e320ac736a5981d0dde787b703b3a1cd
Ray <louis383@gmail.com>
2017-08-30 20:04:03 +0800

    Main Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public string playLevel;
+    
+    public void StartGame() {
+        SceneManager.LoadScene(playLevel);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
