1f6a34e25d7a32e8d768a59fa842eb7b1c4409d8
Ray-Yuan Liu <louis383@gmail.com>
2017-11-11 14:08:14 +0800

    add basic mvc scripts. such as: - `ProjectApplicaion` base Application class. - `ProjectComponents` every elements extends this class to get Application reference. - `ProjectModel` base model class. it contained every model class reference. - `ProjectController` base controller class. it contained every controller class reference. - `ProjectView` base view class. it contained every view class reference. --- Update Unity version to 2017.02f.

---
+++ Assets/Scripts/ProjectApplication.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectApplication : MonoBehaviour {
+
+
+
+    // Use this for initialization
+    void Start() { }
+
+    // Update is called once per frame
+    void Update() { }
+}
---
+++ Assets/Scripts/ProjectComponents.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectComponents : MonoBehaviour {
+    public ProjectApplication app {
+        get {
+            return FindObjectOfType<ProjectApplication>();
+        }
+    }
+}
