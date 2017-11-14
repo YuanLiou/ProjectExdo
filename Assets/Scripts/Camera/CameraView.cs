39da615600ccf11d187a8747dde15fa06bdc6b53
Ray-Yuan Liu <louis383@gmail.com>
2017-11-14 12:31:54 +0800

    `Camera` refactor into `CameraView` and `CameraController`

---
+++ Assets/Scripts/Camera/CameraView.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class CameraView : ProjectComponent {
+
+    public void MoveToRight(float moveDistances) {
+        transform.position = new Vector3(
+            transform.position.x + moveDistances,
+            transform.position.y,
+            transform.position.z
+            );
+    }
+}
--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour {
-	
-    public PlayerController player;
+public class CameraController : ProjectComponent {
+
+    public CameraView mainCameraView;
+    public PlayerController player;    // TODO:: Refactor to PlayerView
     private Vector3 lastPlayerPosition;
     private float distanceToMove;
 	
@@ -16,7 +17,7 @@ public class CameraController : MonoBehaviour {
     // Update is called once per frame
     void Update () {
         distanceToMove = player.transform.position.x - lastPlayerPosition.x;
-        transform.position = new Vector3(transform.position.x + distanceToMove, transform.position.y, transform.position.z);
         lastPlayerPosition = player.transform.position;
+        mainCameraView.MoveToRight(distanceToMove);
     }
 }
